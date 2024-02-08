d0bbcd29332f76ace3938f0658fb81046e8ae533
Priyabrath Tripathi <priyavrathtiwariluck@gmail.com>
2024-02-08 11:23:51 +0530

    Blog App

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-3xl text-cyan-300'>This is blog app</h1>
+      
+    </div>
+  )
+}
+
+export default App
