ec5afd1c0b3abce31df448a4185a479ae60a8747
Priyabrath Tripathi <priyavrathtiwariluck@gmail.com>
2024-02-11 11:54:02 +0530

    dashboard privateroute

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,22 +8,25 @@ import SignUp from './pages/SignUp'
 import Projects from './pages/Projects'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import PrivateRoute from './components/PrivateRoute'
 
 
 const App = () => {
   return (
     <BrowserRouter>
-    <Header/>
+      <Header />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/about' element={<About />} />
         <Route path='/sign-in' element={<SignIn />} />
         <Route path='sign-up' element={<SignUp />} />
-        <Route path='/dashboards' element={<Dashboards />} />
-        <Route path='/projects' element={<Projects/>}/>
+        <Route element={<PrivateRoute />}>
+          <Route path='/dashboards' element={<Dashboards />} />
+        </Route>
+        <Route path='/projects' element={<Projects />} />
 
       </Routes>
-       <Footer/>
+      <Footer />
     </BrowserRouter>
   )
 }
