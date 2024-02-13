fc609d12cee1e441bd29f7bb14e97f062c256d57
Priyabrath Tripathi <priyavrathtiwariluck@gmail.com>
2024-02-13 14:17:23 +0530

    admin functionality

--- api/models/user.model.js
+++ api/models/user.model.js
@@ -21,6 +21,10 @@ const userSchema = new mongoose.Schema(
       default:
         "https://imgs.search.brave.com/MWlI8P3aJROiUDO9A-LqFyca9kSRIxOtCg_Vf1xd9BA/rs:fit:860:0:0/g:ce/aHR0cHM6Ly90NC5m/dGNkbi5uZXQvanBn/LzAyLzE1Lzg0LzQz/LzM2MF9GXzIxNTg0/NDMyNV90dFg5WWlJ/SXllYVI3TmU2RWFM/TGpNQW15NEd2UEM2/OS5qcGc",
     },
+    isAdmin:{
+      type:Boolean,
+      default:false,
+    },
   },
   { timestamps: true }
 );
