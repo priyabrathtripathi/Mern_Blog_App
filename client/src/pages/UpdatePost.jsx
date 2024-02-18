f281a5e7ea5a0f691432cc539c85bbc24db4f275
Priyabrath Tripathi <priyavrathtiwariluck@gmail.com>
2024-02-18 11:48:31 +0530

    update post

--- client/src/pages/UpdatePost.jsx
+++ client/src/pages/UpdatePost.jsx
@@ -133,6 +133,7 @@ export default function UpdatePost() {
             <option value='javascript'>JavaScript</option>
             <option value='reactjs'>React.js</option>
             <option value='nextjs'>Next.js</option>
+            <option value='mernstack'>MERN Stack</option>
           </Select>
         </div>
         <div className='flex gap-4 items-center justify-between border-4 border-teal-500 border-dotted p-3'>
